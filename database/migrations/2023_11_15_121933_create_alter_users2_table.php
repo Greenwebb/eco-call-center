4b219e56d47270ec09d080f1762b94ceabe303ef
unknown <theegreenweb@gmail.com>
2023-11-15 19:04:20 +0200

    advisory | BPO

--- database/migrations/2023_11_15_121933_create_alter_users2_table.php
+++ database/migrations/2023_11_15_121933_create_alter_users2_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('global_key')->unique();
+            $table->string('global_key')->nullable();
         });
     }
 
